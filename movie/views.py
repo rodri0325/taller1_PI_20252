190a5b8fb43434942016c63fa6065f2411b5d6f8
David Rodríguez <drodrige10@eafit.edu.co>
2025-07-30 20:06:36 -0500

    primer commit

---
+++ movie/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    #return HttpResponse('<h1>Welcome to home page</h1>')
+    #return render(request,'home.html')
+    return render(request, 'home.html', {'name': 'Greg Lim'})
+
+
+def about(request):
+    return render(request, 'about.html')
